428ae77b9175e82189e7cbe3f40aa99e54fbf4c8
debb <debora.leticia11.dl@gmail.com>
2025-09-10 15:19:08 -0300

    primeiro commit

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiDeboraApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiDeboraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiDeboraApplication.class, args);
+	}
+
+}
