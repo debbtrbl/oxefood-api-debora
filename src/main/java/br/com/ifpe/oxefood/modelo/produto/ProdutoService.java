783bfb58a11556bd7939addc5b83ffb1c9c4e721
debb <debora.leticia11.dl@gmail.com>
2025-09-17 17:12:27 -0300

    terminei produto

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -1,5 +1,21 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoService {
-    
+
+   @Autowired
+   private ProdutoRepository repository; 
+
+   @Transactional
+   public Produto save(Produto produto) {
+
+       produto.setHabilitado(Boolean.TRUE); // habilita o produto por padrão
+       return repository.save(produto); // insere no banco de dados
+   }
+
 }
