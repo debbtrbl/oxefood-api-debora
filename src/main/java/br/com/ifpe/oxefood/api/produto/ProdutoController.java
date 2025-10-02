dbfca3a2a57b1f4b157ca93b7ec967405778835c
debb <debora.leticia11.dl@gmail.com>
2025-10-01 23:00:59 -0300

    relacionamento produto e categoria

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ifpe.oxefood.api.entregador.EntregadorRequest;
-import br.com.ifpe.oxefood.modelo.entregador.Entregador;
+import br.com.ifpe.oxefood.modelo.categoriaProduto.CategoriaProdutoService;
 import br.com.ifpe.oxefood.modelo.produto.Produto;
 import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
 
@@ -28,11 +27,17 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+    @Autowired
+    private CategoriaProdutoService categoriaProdutoService;
+
+
     @PostMapping
     public ResponseEntity<Produto> save(@RequestBody ProdutoRequest request) {
 
-        Produto produto = produtoService.save(request.build());
-        return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
+       Produto produtoNovo = request.build();
+       produtoNovo.setCategoria(categoriaProdutoService.obterPorID(request.getIdCategoria()));
+       Produto produto = produtoService.save(produtoNovo);
+       return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -48,7 +53,9 @@ public class ProdutoController {
     @PutMapping("/{id}")
     public ResponseEntity<Produto> update(@PathVariable("id") Long id, @RequestBody ProdutoRequest request) {
 
-        produtoService.update(id, request.build());
+        Produto produto = request.build();
+        produto.setCategoria(categoriaProdutoService.obterPorID(request.getIdCategoria()));
+        produtoService.update(id, produto);
         return ResponseEntity.ok().build();
     }
 
