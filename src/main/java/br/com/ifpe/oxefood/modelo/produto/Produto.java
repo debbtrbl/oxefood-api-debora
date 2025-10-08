f89be385895ff9a3d2a6688d4889c8327f65ad0f
debb <debora.leticia11.dl@gmail.com>
2025-10-07 21:46:09 -0300

    add enderecoCliente

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/Produto.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/Produto.java
@@ -6,6 +6,7 @@ import br.com.ifpe.oxefood.modelo.categoriaProduto.CategoriaProduto;
 import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -24,7 +25,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Produto extends EntidadeAuditavel  {
 
-   @ManyToOne
+   @ManyToOne(fetch = FetchType.EAGER)
    private CategoriaProduto categoria;
   
    @Column
