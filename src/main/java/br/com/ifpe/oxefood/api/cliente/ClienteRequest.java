d1a927ac09cc5365e20c0574c04c6faab736ed50
debb <debora.leticia11.dl@gmail.com>
2025-10-14 22:18:10 -0300

    validaçoes nas entradas das requisições

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
@@ -11,6 +11,7 @@ import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,33 +23,35 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ClienteRequest {
 
-   @NotNull(message = "O Nome é de preenchimento obrigatório")
-   @NotEmpty(message = "O Nome é de preenchimento obrigatório")
-   @Length(max = 100, message = "O Nome deverá ter no máximo {max} caracteres")
-   private String nome;
-   
-   @JsonFormat(pattern = "dd/MM/yyyy")
-   private LocalDate dataNascimento;
+    @NotNull(message = "O nome é de preenchimento obrigatório")
+    @NotEmpty(message = "O nome é de preenchimento obrigatório")
+    @Length(max = 100, message = "O nome deverá ter no máximo {max} caracteres")
+    private String nome;
 
-   @NotBlank(message = "O CPF é de preenchimento obrigatório")
-   @CPF
-   private String cpf;
+    @NotBlank(message = "A data de nascimento é de preenchimento obrigatório")
+    @Past(message = "A data de nascimento deve ser uma data passada")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
 
-   @Length(min = 8, max = 20, message = "O campo Fone tem que ter entre {min} e {max} caracteres")
-   private String foneCelular;
+    @NotBlank(message = "O CPF é de preenchimento obrigatório")
+    @CPF
+    private String cpf;
 
-   @Length(min = 8, max = 20, message = "O campo Fone tem que ter entre {min} e {max} caracteres")
-   private String foneFixo;
+    @Length(min = 8, max = 20, message = "O campo telefone tem que ter entre {min} e {max} caracteres")
+    private String foneCelular;
 
-   public Cliente build() {
+    @Length(min = 8, max = 20, message = "O campo telefone fixo tem que ter entre {min} e {max} caracteres")
+    private String foneFixo;
 
-       return Cliente.builder() //instanciando um objeto da classe Cliente
-           .nome(nome)
-           .dataNascimento(dataNascimento)
-           .cpf(cpf)
-           .foneCelular(foneCelular)
-           .foneFixo(foneFixo)
-           .build();
-   }
+    public Cliente build() {
+
+        return Cliente.builder() // instanciando um objeto da classe Cliente
+                .nome(nome)
+                .dataNascimento(dataNascimento)
+                .cpf(cpf)
+                .foneCelular(foneCelular)
+                .foneFixo(foneFixo)
+                .build();
+    }
 
 }
--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -1,6 +1,11 @@
 package br.com.ifpe.oxefood.api.produto;
 
+import org.hibernate.validator.constraints.Length;
+
 import br.com.ifpe.oxefood.modelo.produto.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,33 +17,37 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProdutoRequest {
 
-   private Long idCategoria;
+    @NotNull(message = "O idCategoria é de preenchimento obrigatório")
+    private Long idCategoria;
 
-   private String codigo;
+    @NotBlank(message = "O código é de preenchimento obrigatório")
+    private String codigo;
 
+    @NotBlank(message = "O título é de preenchimento obrigatório")
+    @Length(max = 100, message = "O título deverá ter no máximo {max} caracteres")
+    private String titulo;
 
-   private String titulo;
+    @NotBlank(message = "A descrição é de preenchimento obrigatório")
+    private String descricao;
 
+    @NotBlank(message = "O valor unitário é de preenchimento obrigatório")
+    private double valorUnitario;
 
-   private String descricao;
+    @NotBlank(message = "O tempo de entrega mínimo é de preenchimento obrigatório")
+    private int tempoEntregaMinimo;
 
+    @NotBlank(message = "O tempo de entrega máximo é de preenchimento obrigatório")
+    private int tempoEntregaMaximo;
 
-   private double valorUnitario;
+    public Produto build() {
 
-
-   private int tempoEntregaMinimo;
-
-   private int tempoEntregaMaximo;
-
-   public Produto build() {
-
-       return Produto.builder() //instanciando um objeto da classe Produto
-           .codigo(codigo)
-           .titulo(titulo)
-           .descricao(descricao)
-           .valorUnitario(valorUnitario)
-           .tempoEntregaMinimo(tempoEntregaMinimo)
-           .tempoEntregaMaximo(tempoEntregaMaximo)
-           .build();
-   }
+        return Produto.builder() // instanciando um objeto da classe Produto
+                .codigo(codigo)
+                .titulo(titulo)
+                .descricao(descricao)
+                .valorUnitario(valorUnitario)
+                .tempoEntregaMinimo(tempoEntregaMinimo)
+                .tempoEntregaMaximo(tempoEntregaMaximo)
+                .build();
+    }
 }
